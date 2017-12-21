151523f28b6b83b8b161ebbeeaabfe6b15f0fe13
NandoRufino <nando.rufino@discovercard.com.br>
2017-12-21 01:28:50 -0200

    Add files via upload
    Projeto iniciado

---
+++ AbrirConta.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abrirconta;
+
+/**
+ *
+ * @author Nando
+ */
+public class AbrirConta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        conta c1 = new conta("CC",0.0f);
+        
+        c1.statusConta();
+        //c1.setNumconta(0);
+        
+    }
+    
+}
